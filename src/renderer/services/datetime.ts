17197823369ea6292d1311646febe9686e9b0960
Dan Corin <dcorin6@gmail.com>
2024-12-06 11:31:52 -0500

    Remove datetime log

--- src/renderer/services/datetime.ts
+++ src/renderer/services/datetime.ts
@@ -1,7 +1,6 @@
 export function formatDateTime(date: string | Date) {
     const utcDate = new Date(date);
     const localDate = new Date(utcDate);
-    console.log(`Converting ${date} to local time ${localDate}`);
     return `${localDate.toLocaleDateString("en-CA")} ${
         localDate.toLocaleTimeString("en-US", {
             hour: "numeric",
