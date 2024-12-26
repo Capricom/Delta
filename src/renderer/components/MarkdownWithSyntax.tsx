13462802967d8055a4656c3748d79f73f592dd27
Dan Corin <dcorin6@gmail.com>
2024-12-26 18:39:17 -0500

    Restyle markdown

--- src/renderer/components/MarkdownWithSyntax.tsx
+++ src/renderer/components/MarkdownWithSyntax.tsx
@@ -2,8 +2,8 @@ import React from 'react';
 import ReactMarkdown from 'react-markdown';
 import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter';
 import { dracula } from 'react-syntax-highlighter/dist/cjs/styles/prism';
-
 import '@xyflow/react/dist/style.css';
+import remarkGfm from 'remark-gfm';
 
 interface MarkdownWithSyntaxProps {
     children: string;
@@ -18,8 +18,9 @@ const MarkdownWithSyntax = ({ children, showRaw = false }: MarkdownWithSyntaxPro
 
     return (
         <ReactMarkdown
+            remarkPlugins={[remarkGfm]}
             components={{
-                code({ node, className, children, ...props }) {
+                code: ({ node, className, children, ...props }) => {
                     const match = /language-(\w+)/.exec(className || '');
                     return match ? (
                         <SyntaxHighlighter
@@ -27,12 +28,12 @@ const MarkdownWithSyntax = ({ children, showRaw = false }: MarkdownWithSyntaxPro
                             language={match[1]}
                             PreTag="div"
                             className="[&::-webkit-scrollbar]:w-2 [&::-webkit-scrollbar]:h-2 [&::-webkit-scrollbar-track]:bg-transparent [&::-webkit-scrollbar-thumb]:bg-gray-300 dark:[&::-webkit-scrollbar-thumb]:bg-gray-600 [&::-webkit-scrollbar-thumb]:rounded-full"
-                            {...(props as any)}
+                            {...props as any}
                         >
                             {String(children).replace(/\n$/, '')}
                         </SyntaxHighlighter>
                     ) : (
-                        <code className={className} {...props}>
+                        <code className="bg-gray-200 dark:bg-gray-700 px-1.5 py-0.5 rounded text-sm" {...props}>
                             {children}
                         </code>
                     );
@@ -40,7 +41,20 @@ const MarkdownWithSyntax = ({ children, showRaw = false }: MarkdownWithSyntaxPro
                 p: ({ children }) => <p className="my-1">{children}</p>,
                 ul: ({ children }) => <ul className="my-1 pl-4 list-disc">{children}</ul>,
                 ol: ({ children }) => <ol className="my-1 pl-4 list-decimal">{children}</ol>,
-                li: ({ children, ...props }) => {
+                li: ({ children, ...props }: any) => {
+                    if (props.checked !== null && props.checked !== undefined) {
+                        return (
+                            <li className="my-0.5 ml-4 flex items-center gap-2 list-none" {...props}>
+                                <input
+                                    type="checkbox"
+                                    checked={props.checked}
+                                    readOnly
+                                    className="mt-1"
+                                />
+                                <span>{children}</span>
+                            </li>
+                        );
+                    }
                     return (
                         <li className="my-0.5 ml-4" {...props}>
                             {children}
@@ -53,10 +67,15 @@ const MarkdownWithSyntax = ({ children, showRaw = false }: MarkdownWithSyntaxPro
                 h4: ({ children }) => <h4 className="text-base font-semibold my-1.5">{children}</h4>,
                 h5: ({ children }) => <h5 className="text-sm font-semibold my-1">{children}</h5>,
                 h6: ({ children }) => <h6 className="text-sm font-medium my-1">{children}</h6>,
-                blockquote: ({ children }) => <blockquote className="my-0.5 pl-2 border-l-2 border-gray-400">{children}</blockquote>,
+                blockquote: ({ children }) => (
+                    <blockquote className="my-0.5 pl-4 border-l-4 border-gray-400 bg-gray-100 dark:bg-gray-800 py-1">
+                        {children}
+                    </blockquote>
+                ),
                 hr: () => <hr className="my-1" />,
                 table: ({ children }) => <table className="my-0.5">{children}</table>,
                 pre: ({ children }) => <pre className="my-0.5">{children}</pre>,
+                em: ({ children }) => <em className="italic">{children}</em>,
             }}
             className="[&>*:first-child]:mt-0 [&>*:last-child]:mb-0"
         >
